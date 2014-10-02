c201cc63faaf264b4a531e4345e96b3a6235f5e1
Cédric Bellegarde <gnumdk@gmail.com>
2014-10-02 17:53:35 +0200

    Initial import based on gnome music

---
+++ autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ACLOCAL_FLAGS="-I libgd ${ACLOCAL_FLAGS}"
+PKG_NAME="yaelle"
+
+test -f $srcdir/configure.ac || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level yaelle directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from GNOME Git (or from"
+    echo "your OS vendor's package manager)."
+    exit 1
+}
+
+touch AUTHORS
+. gnome-autogen.sh
