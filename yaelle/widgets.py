388c701f8a936358a68e0c3a9ec8b0a5cafa8884
Cédric Bellegarde <gnumdk@gmail.com>
2014-10-07 18:53:22 +0200

    Replace Grid by FlowBox

--- yaelle/widgets.py
+++ yaelle/widgets.py
@@ -49,7 +49,12 @@ class AlbumWidgetSongs(Gtk.Grid):
 		self._db = db
 		self._player = player
 		self._art = AlbumArt(db)
-		
+	
+		flowbox = self._ui.get_object('flow')
+		nb_tracks = self._db.get_tracks_count_for_album(album_id)
+		flowbox.set_property("min-children-per-line", nb_tracks/3)
+		flowbox.set_property("max-children-per-line", nb_tracks/3)
+		flowbox.set_selection_mode(Gtk.SelectionMode.NONE)
 		self._player.connect("current-changed", self._update_tracks)
 		
 		self._ui.get_object('cover').set_from_pixbuf(self._art.get(album_id))
@@ -59,8 +64,6 @@ class AlbumWidgetSongs(Gtk.Grid):
 	
 
 	def _add_tracks(self, album_id):
-		tracks = self._db.get_tracks_count_for_album(album_id)
-		i = 0
 		for (id, name, filepath, length, year) in self._db.get_songs_by_album(album_id):
 			ui = Gtk.Builder()
 			ui.add_from_resource('/org/gnome/Yaelle/TrackWidget.ui')
@@ -80,16 +83,10 @@ class AlbumWidgetSongs(Gtk.Grid):
 				song_widget.title.set_markup('<b>%s</b>' % name)
 
 			ui.get_object('title').set_alignment(0.0, 0.5)
-			self._ui.get_object('grid1').attach(
-					song_widget,
-					int(i / (tracks / 3)),
-					int(i % (tracks / 3)), 1, 1)
+			self._ui.get_object('flow').insert(song_widget, -1)
 			song_widget.checkButton = ui.get_object('select')
 			song_widget.checkButton.set_visible(False)
-
-
 			song_widget.show_all()
-			i+=1
 			
 	def _track_selected(self, widget, data):
 		for id, song_widget in self._songs:
